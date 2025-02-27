f9d83a207e50f593d9794304e588980df6dcda15
yves3a <yves.irakoz3@gmail.com>
2025-02-27 19:29:05 +0200

    updates

--- sort.h
+++ sort.h
@@ -8,7 +8,6 @@
 #include <stddef.h>
 #include <stdbool.h>
 
-
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -18,11 +17,10 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
-
 
 /* Function prototypes */
 void print_list(const listint_t *list);
@@ -30,6 +28,5 @@ void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
-
 
 #endif /* SORT_H */
