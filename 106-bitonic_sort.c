def769c2aa95886532299aefd63dc45595b0c2f6
yves3a <yves.irakoz3@gmail.com>
2025-02-27 21:56:56 +0200

    Updates

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -2,6 +2,21 @@
 #include <stdio.h>
 
 /**
+ * swap_elements - Swaps two elements in an array
+ * @array: Array containing elements
+ * @i: First index
+ * @j: Second index
+ */
+void swap_elements(int *array, size_t i, size_t j)
+{
+	int temp;
+
+	temp = array[i];
+	array[i] = array[j];
+	array[j] = temp;
+}
+
+/**
  * bitonic_compare - Compare and swap elements in bitonic sequence
  * @array: Array to sort
  * @start: Starting index of sequence
@@ -12,19 +27,11 @@ void bitonic_compare(int *array, size_t start, size_t seq, int dir)
 {
 	size_t jump = seq / 2;
 	size_t i;
-	int temp;
 
 	for (i = start; i < start + jump; i++)
 	{
 		if ((array[i] > array[i + jump]) == dir)
-		{
-			temp = array[i];
-			array[i] = array[i + jump];
-			array[i + jump] = temp;
-			printf("Merging [%lu/%lu] (%s):\n", seq, seq,
-					dir ? "UP" : "DOWN");
-			print_array(array + start, seq);
-		}
+			swap_elements(array, i, i + jump);
 	}
 }
 
@@ -34,16 +41,21 @@ void bitonic_compare(int *array, size_t start, size_t seq, int dir)
  * @start: Starting index of sequence
  * @seq: Length of sequence
  * @dir: Direction of sorting (1 for ascending, 0 for descending)
+ * @size: Total size of original array
  */
-void bitonic_merge(int *array, size_t start, size_t seq, int dir)
+void bitonic_merge(int *array, size_t start, size_t seq, int dir, size_t size)
 {
 	size_t k = seq / 2;
 
 	if (seq > 1)
 	{
+		printf("Merging [%lu/%lu] (%s):\n", seq, size,
+				dir ? "UP" : "DOWN");
+		print_array(array + start, seq);
+
 		bitonic_compare(array, start, seq, dir);
-		bitonic_merge(array, start, k, dir);
-		bitonic_merge(array, start + k, k, dir);
+		bitonic_merge(array, start, k, dir, size);
+		bitonic_merge(array, start + k, k, dir, size);
 	}
 }
 
@@ -67,7 +79,7 @@ void bitonic_seq(int *array, size_t start, size_t seq, int dir, size_t size)
 
 		bitonic_seq(array, start, k, 1, size);
 		bitonic_seq(array, start + k, k, 0, size);
-		bitonic_merge(array, start, seq, dir);
+		bitonic_merge(array, start, seq, dir, size);
 	}
 }
 
