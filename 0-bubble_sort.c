f048fa77233ba1f1b5d2db66e055bdd724cb18a8
yves3a <yves.irakoz3@gmail.com>
2025-02-27 19:21:42 +0200

    Selection sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,6 +1,20 @@
 #include "sort.h"
 
 /**
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
  * bubble_sort - Sorts an array of integers in
  *                  ascending order using Bubble Sort
  * @array: The array to be sorted
